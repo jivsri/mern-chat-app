087c645b6ad9cc2a712624c3c92c28ee77a11f2f
Jivtesh <jiv99sri25@gmail.com>
2024-07-04 20:26:07 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { app, server } from "./socket/socket.js";
-
+import path from "path";
 const port=5000 || process.env.PORT;
 
-
+const __dirname=path.resolve();
 
 dotenv.config();
 
@@ -17,21 +17,25 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
 
 
-// app.get("/",(req,res)=>{
-//     res.send("Noice working1");
-// });
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 
 
 
 server.listen(port,()=>{
     connectToDB();
+    // console.log(p);
     console.log(`server connected at ${port}`);
 })
